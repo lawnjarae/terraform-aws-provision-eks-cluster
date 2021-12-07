3856add9967dcf82f99b23f424baccbe9e553396
Justin Jarae <lawnjarae@gmail.com>
2021-12-07 14:09:42 -0700

    Updated the dashboard script.

--- add-dashboard-to-kube.sh
+++ add-dashboard-to-kube.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+  then
+    echo "Missing param: cluster-name"
+fi
+
 # Set the kubectl context
-aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name)
+aws eks --region "us-west-2" update-kubeconfig --name $1
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
 kubectl apply -f dashboard-adminuser.yaml 
 kubectl apply -f cluster-role-binding.yaml
+
+./get-dashboard-token.sh
+
+kubectl proxy
